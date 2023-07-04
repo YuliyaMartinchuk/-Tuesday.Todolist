f88d5a318055881633d052de7a6448bc8bbdfe90
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-07-04 23:50:20 +0300

    lesson 13 (Thursday week1. added type for instance.(get,post,delete,put)

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
+
 
 
 const instance = axios.create(
@@ -11,17 +12,47 @@ const instance = axios.create(
 
 export const TodolistApi = {
     getTodolist() {
-        return instance.get('todo-lists')
+        return instance.get<TodolistType[]>('todo-lists')
     },
-    createTodoLists(title:string) {
-        return instance.post('todo-lists', {title})
+    createTodoLists(title: string) {
+        return instance.post<ResponseType<{ items: TodolistType }>, AxiosResponse<ResponseType<{ items: TodolistType }>>, { title: string }>('todo-lists', {title})
     },
-   deleteTodolist(todolistId:string) {
-        return instance.delete(`todo-lists/${todolistId}`)
+    deleteTodolist(todolistId: string) {
+        return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
     },
-   updateTodolist(todolistId:string, title:string) {
-        return instance.put(`todo-lists/${todolistId}`,{title} )
+    updateTodolist(todolistId: string, title: string) {
+        return instance.put<ResponseType, AxiosResponse<ResponseType>, { title: string }>(`todo-lists/${todolistId}`, {title})
     },
 
 }
 
+type TodolistType = {
+    id: string
+    title: string
+    addedDate: Date
+    order: number
+}
+
+type ResponseType<T = {}> = {
+    resultCode: number
+    messages: string[]
+    data: T
+}
+
+// type CreateTodoListsResponseType<T={items:TodolistType}> = {
+//     resultCode: number
+//     messages: string[]
+//     data: T
+// }
+//
+// type DeleteTodolistResponseType<T={}> = {
+//     resultCode: number
+//     messages: string[]
+//     data: T
+// }
+//
+// type UpdateTodolistResponseType<T={}> = {
+//     resultCode: number
+//     messages: string[]
+//     data: T
+// }
